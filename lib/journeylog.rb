4f1f657ceaa7f5b51e6b5c135c4039d23290d56c
Claudia Green <claudia@claudiag.me.uk>
2016-03-11 16:21:29 +0000

    Working on finish method for #15

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -3,21 +3,17 @@ require_relative 'journey'
 class JourneyLog
 
   def initialize(journey_class = Journey)
-    @journey_class = journey_class
-    # @journeys = []
-  end
+     @journey_class = journey_class
+    #  @journeys = []
+   end
 
   def start(entry_station)
     @current_journey = @journey_class.new(entry_station)
   end
 
   def finish(exit_station)
-    # @current_journey = @journey_class(exit_station)
+    @current_journey = @journey_class.new(exit_station)
     # @journeys << @current_journey
-  end
-
-  def complete?
-    !(exit_station == nil || entry_station == nil)
   end
 
   private
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -4,28 +4,26 @@ require 'journey'
 
 describe JourneyLog do
 
-  subject(:journeylog) {described_class.new(journey_class)}
-  let(:journey_class){double :journey_class, new: journey}
+  subject(:journeylog){described_class.new(journey_class)}
   let(:journey){ double :journey }
-  let(:entry_station){double(:station)}
-  let(:exit_station){double(:station)}
+  let(:station){ double :station }
+  let(:journey_class){double :journey_class, new: journey}
 
-  it {is_expected.to respond_to(:start).with(1).argument}
-  it {is_expected.to respond_to(:finish).with(1).argument}
-
-  describe "#start" do
-    it "starts a new journey" do
-      expect(journey_class).to receive(:new).with(entry_station)
-      journeylog.start(entry_station)
+  describe '#start' do
+    it 'starts a journey' do
+      expect(journey_class).to receive(:new)
+      journeylog.start(station)
     end
   end
 
-  describe "#finish" do
-    it "adds an exit station to the Journey" do
-      journeylog.start(entry_station)
-      journeylog.finish(exit_station)
-      expect(journeylog).to receive(:complete?).and_return true
+  describe '#finish' do
+    it 'finishes a journey' do
+      journeylog.start(station)
+      journeylog.finish(station)
+      expect(journeylog).to be_a(Journey)
     end
   end
+
+
 
 end
